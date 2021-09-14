cdb37fffe43f04fa1575bd46694bb82ed221fd4e
Sebastian Riga <sebastian.riga.development@gmail.com>
2021-09-14 22:57:21 +0200

    Updated build_web.sh to include rustup for target wasmbindgen | Fixed case errors of github secrets in deploy_web.yaml

--- web/scripts/build_web.sh
+++ web/scripts/build_web.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Adding wasm dependencies..."
+
+rustup target add wasm32-unknown-unknown
+
 echo "Building project for wasm32-unknown-unknown target to prepare web assembly..."
 
 cargo build --release --target wasm32-unknown-unknown 
